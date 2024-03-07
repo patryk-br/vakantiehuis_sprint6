6d866786dd247c5a25e1b3da96d07a6f11877001
patryk-br <pattbrb@gmail.com>
2024-03-07 10:01:58 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="logo">
+            <!-- Add your logo here -->
+        </div>
+        <div class="search-bar">
+            <!-- Add your search bar here -->
+        </div>
+        <div class="nav-links">
+            <a href="#">Home</a>
+            <a href="#">Vakantiewoningen</a>
+            <a href="#">Omgeving</a>
+            <a href="#">Contact</a>
+        </div>
+        <div class="login-link">
+            <a href="inloggen.php">Inloggen</a>
+        </div>
+    </div>
+</body>
+</html>
